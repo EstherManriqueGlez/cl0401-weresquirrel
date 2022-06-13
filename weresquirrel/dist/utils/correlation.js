f0107c5a1445a89063e96b90486c956f41f5db43
EstherManrique <esmango0610@gmail.com>
2022-06-12 19:54:19 -0500

    Added folders exercises and weresquirrel

---
+++ weresquirrel/dist/utils/correlation.js
@@ -0,0 +1,30 @@
+import JOURNAL from './JOURNAL.js';
+function addEntry(events, squirrel) {
+    JOURNAL.push({ events, squirrel });
+}
+function phi([n00, n01, n10, n11]) {
+    return (n11 * n00 - n10 * n01) /
+        Math.sqrt((n10 + n11) * (n00 + n01) *
+            (n01 + n11) * (n00 + n10));
+}
+function tableFor(event, journal) {
+    let table = [0, 0, 0, 0];
+    for (let entry of journal) {
+        let index = 0;
+        if (entry.events.includes(event))
+            index += 1;
+        if (entry.squirrel)
+            index += 2;
+        table[index] += 1;
+    }
+    return table;
+}
+function journalEvents(journal) {
+    let events = [];
+    for (let entry of journal)
+        for (let event of entry.events)
+            if (!events.includes(event))
+                events.push(event);
+    return events;
+}
+export { addEntry, phi, tableFor, journalEvents };
